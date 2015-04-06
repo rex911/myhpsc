c3eccc211100331f5b840231ee206d7bc23cc37a
Ubuntu <ubuntu@ip-10-0-0-219.ec2.internal>
2015-04-06 23:33:46 +0000

    added intersections.py

--- homework3/intersections.py
+++ homework3/intersections.py
@@ -1,5 +1,6 @@
 from numpy import nan
 from numpy import sqrt
+from numpy import sin, cos
 from scipy.optimize import fsolve
 
 epsilon = 10**(-14)
@@ -9,14 +10,14 @@ def solve(fvals, x0, debug=False):
     if x0 == 0: return 0
     x = x0
     if debug:
-        print 'Initial guess: x = %f' %x
+        print 'Initial guess: x = %22.15e' %x
     for i in range(max_iter):
-        if abs(x - fsolve(lambda x: x**2-4, x0)) < epsilon:
+        if abs(x - fsolve(lambda x: sin(x)-1+x**2, x0)) < epsilon:
             return x, i
         f, fp = fvals(x)
         x = x - f/fp
         if debug:
-            print 'After %d iterations, x = %f' %(i+1, x)
+            print 'After %d iterations, x = %22.15e' %(i+1, x)
     return x, max_iter
 # $UWHPSC/codes/homework3/test_code.py 
 # To include in newton.py
@@ -25,8 +26,8 @@ def fvals_sqrt(x):
     """
     Return f(x) and f'(x) for applying Newton to find a square root.
     """
-    f = x**2 - 4.
-    fp = 2.*x
+    f = sin(x)-1+x**2
+    fp = cos(x) + 2*x
     return f, fp
 
 def test1(debug_solve=False):
@@ -34,8 +35,7 @@ def test1(debug_solve=False):
     Test Newton iteration for the square root with different initial
     conditions.
     """
-    from numpy import sqrt
-    for x0 in [1., 2., 100.]:
+    for x0 in [-0.5, 0.5]:
         print " "  # blank line
         x,iters = solve(fvals_sqrt, x0, debug=debug_solve)
         print "solve returns x = %22.15e after %i iterations " % (x,iters)
