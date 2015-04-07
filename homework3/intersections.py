adce3f258c69892cb3f71e2114e80eb6cc60f81a
Ubuntu <ubuntu@ip-10-0-0-219.ec2.internal>
2015-04-07 00:46:30 +0000

    intersections.py now works properly

--- homework3/intersections.py
+++ homework3/intersections.py
@@ -1,18 +1,19 @@
+import numpy as np
 from numpy import nan
 from numpy import sqrt
-from numpy import sin, cos
+from numpy import sin, cos, pi
 from scipy.optimize import fsolve
+import matplotlib.pyplot as plt
+
 
 epsilon = 10**(-14)
 max_iter = 20
 def solve(fvals, x0, debug=False):
-    if x0 < 0: return nan
-    if x0 == 0: return 0
     x = x0
     if debug:
         print 'Initial guess: x = %22.15e' %x
     for i in range(max_iter):
-        if abs(x - fsolve(lambda x: sin(x)-1+x**2, x0)) < epsilon:
+        if abs(x - fsolve(lambda x: x*cos(pi*x)-1+0.6*x**2, x0)) < epsilon:
             return x, i
         f, fp = fvals(x)
         x = x - f/fp
@@ -26,8 +27,8 @@ def fvals_sqrt(x):
     """
     Return f(x) and f'(x) for applying Newton to find a square root.
     """
-    f = sin(x)-1+x**2
-    fp = cos(x) + 2*x
+    f = x*cos(pi*x)-1+0.6*x**2
+    fp = cos(pi*x)-sin(pi*x)*pi*x + 1.2*x
     return f, fp
 
 def test1(debug_solve=False):
@@ -35,13 +36,22 @@ def test1(debug_solve=False):
     Test Newton iteration for the square root with different initial
     conditions.
     """
-    for x0 in [-0.5, 0.5]:
+    for x0 in [-2,-1.5,-1,1.5]:
         print " "  # blank line
+        #import pdb; pdb.set_trace()
         x,iters = solve(fvals_sqrt, x0, debug=debug_solve)
         print "solve returns x = %22.15e after %i iterations " % (x,iters)
         fx,fpx = fvals_sqrt(x)
         print "the value of f(x) is %22.15e" % fx
-        assert abs(x-2.) < 1e-14, "*** Unexpected result: x = %22.15e"  % x
+        assert abs(x-fsolve(lambda x: x*cos(pi*x)-1+0.6*x**2, x0)) < 1e-14, "*** Unexpected result: x = %22.15e"  % x
+        plt.plot(x, 1-0.6*x**2, 'ko')
+
+    # plot curves of the 2 functions
+    x = np.linspace(-5,5,1000)
+    f = x*np.cos(np.pi*x)
+    g=1-0.6*x**2
+    plt.plot(x,f, 'r')
+    plt.plot(x,g, 'b')
 
 
 if __name__ == '__main__':
